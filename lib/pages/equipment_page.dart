486bb8e314c5b0d92eb2899f2e83034467b0eef1
Scott Jordan <jordan.scott.email@gmail.com>
2020-11-29 14:31:10 -0800

    created basic ui for equipment page

--- lib/pages/equipment_page.dart
+++ lib/pages/equipment_page.dart
@@ -1,17 +1,81 @@
 import 'package:flutter/material.dart';
 
-class EquipmentPage extends StatelessWidget {
+import '../pages/workout_setup_page.dart';
+
+class EquipmentPage extends StatefulWidget {
   static const routeName = 'equipment-page';
+
+  @override
+  _EquipmentPageState createState() => _EquipmentPageState();
+}
+
+class _EquipmentPageState extends State<EquipmentPage> {
+  bool _pullupBar;
+  bool _yogaMat;
+
+  @override
+  void initState() {
+    _pullupBar = false;
+    _yogaMat = false;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).accentColor,
         centerTitle: true,
-        title: Text('Workout App'),
+        title: Text('Equipment Page'),
       ),
-      body: Center(
-        child: Text('Equipment Page'),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 100,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Pullup Bar'),
+              Switch(
+                  value: _pullupBar,
+                  onChanged: (value) {
+                    setState(() {
+                      _pullupBar = value;
+                      print(_pullupBar);
+                    });
+                  }),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Yoga Mat'),
+              Switch(
+                  value: _yogaMat,
+                  onChanged: (value) {
+                    setState(() {
+                      _yogaMat = value;
+                      print(_yogaMat);
+                    });
+                  }),
+            ],
+          ),
+          Container(
+            height: 200,
+            width: 200,
+            alignment: Alignment.bottomCenter,
+            child: RaisedButton(
+              onPressed: () {
+                Navigator.pushNamed(
+                  context,
+                  WorkoutSetupPage.routeName,
+                );
+              },
+              child: Text('Next'),
+            ),
+          ),
+        ],
       ),
     );
   }
