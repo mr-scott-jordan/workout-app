2733daf372bbce3a68a0e7200d473081e3c31035
Scott Jordan <jordan.scott.email@gmail.com>
2021-02-12 17:44:42 -0800

    abstracted drawer animation into a seperate widget

--- lib/features/high_intensity_interval/presentation/pages/primary_drawer.dart
+++ lib/features/high_intensity_interval/presentation/pages/primary_drawer.dart
@@ -1,47 +1,47 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// import '../widgets/formatted_button.dart';
-// import 'equipment_page.dart';
+import '../widgets/formatted_button.dart';
+import 'equipment_page.dart';
 
-// class PrimaryDrawer extends StatelessWidget {
-//   // final Text text;
-//   // PrimaryDrawer(this.text);
-//   @override
-//   Widget build(BuildContext context) {
-//     return Drawer(
-//         child: Container(
-//       // color: Color(0xFF37474F),
-//       color: Color(0xff212121),
-//       child: Column(
-//         children: [
-//           SizedBox(
-//             height: 250,
-//           ),
-//           FormattedButton(
-//             onPressed: () {
-//               Navigator.pushReplacementNamed(context, '/');
-//             },
-//             buttonText: "Main Menu",
-//           ),
-//           SizedBox(
-//             height: 50,
-//           ),
-//           FormattedButton(
-//             onPressed: () {
-//               Navigator.pushReplacementNamed(context, EquipmentPage.routeName);
-//             },
-//             buttonText: "New Workout",
-//           ),
-//         ],
-//       ),
-//     ));
-//   }
-// }
+class PrimaryDrawer extends StatelessWidget {
+  // final Text text;
+  // PrimaryDrawer(this.text);
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+        child: Container(
+      // color: Color(0xFF37474F),
+      color: Color(0xff212121),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 250,
+          ),
+          FormattedButton(
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, '/');
+            },
+            buttonText: "Main Menu",
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          FormattedButton(
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, EquipmentPage.routeName);
+            },
+            buttonText: "New Workout",
+          ),
+        ],
+      ),
+    ));
+  }
+}
 
-// // child: Center(
-// //           // child: Text('Primary Drawer'),
-// //           child: RaisedButton(
-// //               child: Text('Main Menu'),
-// //               onPressed: () {
-// //                 Navigator.pushNamed(context, '/');
-// //               })),
+// child: Center(
+//           // child: Text('Primary Drawer'),
+//           child: RaisedButton(
+//               child: Text('Main Menu'),
+//               onPressed: () {
+//                 Navigator.pushNamed(context, '/');
+//               })),
--- lib/features/high_intensity_interval/presentation/widgets/page_animation_widget.dart
+++ lib/features/high_intensity_interval/presentation/widgets/page_animation_widget.dart
@@ -1,93 +1,91 @@
-// import 'package:flutter/material.dart';
-// import 'package:workout_app/features/high_intensity_interval/presentation/pages/primary_drawer.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:workout_app/features/high_intensity_interval/presentation/pages/primary_drawer.dart';
 
-// class PageAnimationWidget extends StatefulWidget {
-//   final Widget child;
-//   PageAnimationWidget({@required this.child});
+class PageAnimationWidget extends StatefulWidget {
+  final Widget body;
 
-//   @override
-//   _PageAnimationWidgetState createState() => _PageAnimationWidgetState();
-// }
+  PageAnimationWidget({@required this.body});
+  @override
+  _PageAnimationWidgetState createState() => _PageAnimationWidgetState();
+}
 
-// class _PageAnimationWidgetState extends State<PageAnimationWidget>
-//     with SingleTickerProviderStateMixin {
-//   AnimationController _animationController;
+class _PageAnimationWidgetState extends State<PageAnimationWidget>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
 
-//   @override
-//   void initState() {
-//     super.initState();
+  void initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    super.initState();
+  }
 
-//     _animationController =
-//         AnimationController(vsync: this, duration: Duration(milliseconds: 300));
-//   }
+  _toggleAnimation() {
+    _animationController.isDismissed
+        ? _animationController.forward()
+        : _animationController.reverse();
+  }
 
-//   _toggleAnimation() {
-//     _animationController.isDismissed
-//         ? _animationController.forward()
-//         : _animationController.reverse();
-//   }
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
-//   @override
-//   void dispose() {
-//     _animationController.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final rightSlide = MediaQuery.of(context).size.width * 0.6;
-//     // SystemChrome.setEnabledSystemUIOverlays([]);
-
-//     return AnimatedBuilder(
-//       animation: _animationController,
-//       builder: (context, child) {
-//         double slide = rightSlide * _animationController.value;
-//         double scale = 1 - (_animationController.value * 0.3);
-//         return Stack(
-//           children: [
-//             Scaffold(
-//               backgroundColor: const Color(0xff424242),
-//               body: PrimaryDrawer(),
-//             ),
-//             Transform(
-//               transform: Matrix4.identity()
-//                 ..translate(slide)
-//                 ..scale(scale),
-//               alignment: Alignment.center,
-//               child: Scaffold(
-//                 appBar: PreferredSize(
-//                   preferredSize: Size.fromHeight(25),
-//                   child: Container(
-//                     decoration: BoxDecoration(
-//                         gradient: LinearGradient(
-//                             begin: Alignment.topRight,
-//                             end: Alignment.bottomLeft,
-//                             colors: [Colors.blue[400], Colors.purple])),
-//                     // color: const Color(0xff212121),
-//                     child: Padding(
-//                       padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
-//                       child: Column(
-//                         crossAxisAlignment: CrossAxisAlignment.start,
-//                         mainAxisAlignment: MainAxisAlignment.start,
-//                         children: [
-//                           IconButton(
-//                             onPressed: () => _toggleAnimation(),
-//                             icon: AnimatedIcon(
-//                               icon: AnimatedIcons.menu_close,
-//                               progress: _animationController,
-//                             ),
-//                           ),
-//                         ],
-//                       ),
-//                     ),
-//                   ),
-//                 ),
-//                 body: widget.child,
-//               ),
-//             ),
-//           ],
-//         );
-//       },
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
+    final rightSlide = MediaQuery.of(context).size.width * 0.6;
+    final height = MediaQuery.of(context).size.height;
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, child) {
+        double slide = rightSlide * _animationController.value;
+        double scale = 1 - (_animationController.value * 0.3);
+        return Stack(children: [
+          Scaffold(
+            backgroundColor: const Color(0xff424242),
+            body: PrimaryDrawer(),
+          ),
+          Transform(
+            transform: Matrix4.identity()
+              ..translate(slide)
+              ..scale(scale),
+            alignment: Alignment.center,
+            child: Scaffold(
+              appBar: PreferredSize(
+                preferredSize: Size(double.infinity, height * 0.09),
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topRight,
+                      end: Alignment.bottomLeft,
+                      colors: [Colors.blue[400], Colors.purple],
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        IconButton(
+                          onPressed: () => _toggleAnimation(),
+                          icon: AnimatedIcon(
+                            icon: AnimatedIcons.menu_close,
+                            progress: _animationController,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              body: widget.body,
+            ),
+          )
+        ]);
+      },
+    );
+  }
+}
