a37cdd17a515038a804a02c64eec0b0cf673803b
Lance Allred <lallred@nevada.unr.edu>
2021-03-01 19:03:25 -0800

    implemented basic circular countdown timer

--- lib/features/high_intensity_interval/presentation/pages/workout_page.dart
+++ lib/features/high_intensity_interval/presentation/pages/workout_page.dart
@@ -1,3 +1,5 @@
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -7,6 +9,7 @@ import '../widgets/page_animation_widget.dart';
 
 class WorkoutPage extends StatelessWidget {
   static const routeName = 'workout-page';
+  CountDownController _controller = CountDownController();
 
   @override
   Widget build(BuildContext context) {
@@ -14,67 +17,63 @@ class WorkoutPage extends StatelessWidget {
       if (state is WorkoutLoadedState) {
         return PageAnimationWidget(
           body: Container(
+            width: MediaQuery.of(context).size.width,
             color: Color(0xff424242),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+              //crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Container(
-                  width: MediaQuery.of(context).size.width * .8,
-                  child: Placeholder(
-                    fallbackHeight: 250,
-                  ),
-                ),
-                SizedBox(
-                  height: 60,
+                CircularCountDownTimer(
+                  duration: state.workout.totalDuration.inSeconds,
+                  initialDuration: 0,
+                  controller: _controller,
+                  width: MediaQuery.of(context).size.width / 2,
+                  height: MediaQuery.of(context).size.height / 2,
+                  ringColor: Colors.grey[300],
+                  ringGradient: null,
+                  fillColor: Colors.purpleAccent[100],
+                  fillGradient: null,
+                  backgroundColor: Colors.purple[500],
+                  backgroundGradient: null,
+                  strokeWidth: 20.0,
+                  strokeCap: StrokeCap.round,
+                  textStyle: TextStyle(
+                      fontSize: 33.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                  textFormat: CountdownTextFormat.HH_MM_SS,
+                  isReverse: true,
+                  isReverseAnimation: true,
+                  isTimerTextShown: true,
+                  autoStart: false,
+                  onStart: () {
+                    print('Countdown Started');
+                  },
+                  onComplete: () {
+                    print('Countdown Ended');
+                  },
                 ),
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Container(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          'WIP',
-                          style: TextStyle(
-                            fontSize: 24,
-                            color: Colors.white,
-                            decorationColor: Colors.amberAccent,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        )),
-                    Container(
-                      alignment: Alignment.centerRight,
-                      // child: Countdown(
-                      //   duration: Duration(
-                      //     minutes: minutes,
-                      //   ),
-                      //   onFinish: () {
-                      //     print('finished!');
-                      //   },
-                      //   builder: (BuildContext ctx, Duration remaining) {
-                      //     return Text(
-                      //       minutes > 9
-                      //           ? '${remaining.toString().substring(2, 7)}'
-                      //           : '${remaining.toString().substring(2, 7)}',
-                      //       style: TextStyle(
-                      //         color: Colors.amberAccent,
-                      //         fontSize: 20,
-                      //         fontWeight: FontWeight.w900,
-                      //       ),
-                      //     );
-                      //   },
-                      // ),
+                    FormattedButton(
+                      onPressed: _controller.start,
+                      buttonText: "Start",
+                    ),
+                    FormattedButton(
+                      onPressed: _controller.pause,
+                      buttonText: "Pause",
+                    ),
+                    FormattedButton(
+                      onPressed: _controller.resume,
+                      buttonText: "Resume",
+                    ),
+                    FormattedButton(
+                      onPressed: _controller.restart,
+                      buttonText: "Restart",
                     ),
                   ],
-                ),
-                SizedBox(
-                  height: 100,
-                ),
-                FormattedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/');
-                  },
-                  buttonText: "Finish Workout",
-                ),
+                )
               ],
             ),
           ),
