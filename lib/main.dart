203b1c2a01618dcbdc3df7ebd0b9eaca458eedc1
nathan-forest-lee <nathanlee@nevada.unr.edu>
2021-03-22 17:19:43 -0700

    added ListOfWorkoutsPage to routenames

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
+import 'package:workout_app/features/high_intensity_interval/presentation/pages/list_of_workouts_page.dart';
 
 import 'core/authentication/authentication_service.dart';
 import 'features/high_intensity_interval/presentation/bloc/workout_bloc.dart';
@@ -57,6 +58,7 @@ class MyApp extends StatelessWidget {
             BulletinBoardPage.routeName: (context) => BulletinBoardPage(),
             WorkoutPage.routeName: (context) => WorkoutPage(),
             SignInPage.routeName: (context) => SignInPage(),
+            ListOfWorkoutsPage.routeName: (context) => ListOfWorkoutsPage(),
           },
         ),
       ),
