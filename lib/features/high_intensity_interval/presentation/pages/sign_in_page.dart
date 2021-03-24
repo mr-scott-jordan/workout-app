c02022152d30e92c7743fb78cf4b6fbe213dff3c
Scott Jordan <jordan.scott.email@gmail.com>
2021-03-24 15:44:39 -0700

    flutter pub get

--- lib/features/high_intensity_interval/presentation/pages/sign_in_page.dart
+++ lib/features/high_intensity_interval/presentation/pages/sign_in_page.dart
@@ -1,13 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:workout_app/features/high_intensity_interval/presentation/pages/home_page.dart';
 
 import '../../../../core/authentication/authentication_service.dart';
 import '../bloc/workout_bloc.dart';
 import '../widgets/formatted_button.dart';
 import '../widgets/page_animation_widget.dart';
 import 'equipment_page.dart';
+import 'home_page.dart';
 
 class SignInPage extends StatelessWidget {
   final TextEditingController emailController = TextEditingController();
