1fd61088d8c103874dad0776815c580f695c7941
Lance Allred <lallred@nevada.unr.edu>
2021-04-05 13:37:21 -0700

    created _buildPage function to fix redundancies and consolidate page

--- lib/features/high_intensity_interval/presentation/pages/workout_page.dart
+++ lib/features/high_intensity_interval/presentation/pages/workout_page.dart
@@ -1,6 +1,8 @@
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:workout_app/features/high_intensity_interval/domain/entities/exercise.dart';
+import 'package:workout_app/features/high_intensity_interval/domain/entities/workout.dart';
 import 'package:workout_app/features/high_intensity_interval/presentation/pages/workout_summary_page.dart';
 import 'package:workout_app/features/high_intensity_interval/presentation/widgets/formatted_button.dart';
 
@@ -11,7 +13,7 @@ import '../widgets/timer.dart';
 class WorkoutPage extends StatelessWidget {
   static const routeName = '/workout-page';
   final CountDownController controller = CountDownController();
-  var exercises;
+  List<Exercise> exercises;
 
   @override
   Widget build(BuildContext context) {
@@ -38,107 +40,17 @@ class WorkoutPage extends StatelessWidget {
             duration: state.workout.exerciseDuration.inSeconds,
           );
         };
-        return PageAnimationWidget(
-          body: Container(
-            width: MediaQuery.of(context).size.width,
-            color: Color(0xff424242),
-            child: Column(
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text(
-                          'Next Exercise:',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Color(0xfffbc02d),
-                          ),
-                        ),
-                        Text(
-                          ('${exercises.last.title}'),
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      height: 2.0,
-                      width: MediaQuery.of(context).size.width,
-                      color: Colors.purple,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'REST',
-                          style: TextStyle(
-                            fontSize: 45,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                CircleTimer(
-                  duration: state.workout.restDuration.inSeconds,
-                  onComplete: onComplete,
-                  controller: controller,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    IconButton(
-                      onPressed: controller.restart,
-                      icon: Icon(
-                        Icons.replay_rounded,
-                        color: Colors.purple,
-                      ),
-                      iconSize: 75.0,
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        exercises.removeLast();
-                        if (exercises.isEmpty) {
-                          BlocProvider.of<WorkoutBloc>(context)
-                              .add(FinishWorkoutEvent(state.workout));
-                        } else {
-                          BlocProvider.of<WorkoutBloc>(context)
-                              .add(SkipWorkoutEvent(state.workout));
-                          controller.restart(
-                            duration: 10,
-                          );
-                        }
-                      },
-                      icon: Icon(
-                        Icons.skip_next_rounded,
-                        color: Colors.purple,
-                      ),
-                      iconSize: 75.0,
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    FormattedButton(
-                      onPressed: () {
-                        BlocProvider.of<WorkoutBloc>(context)
-                            .add(FinishWorkoutEvent(state.workout));
-                      },
-                      buttonText: 'Finish Workout',
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
+        final pageParams = Params(
+          context: context,
+          workout: state.workout,
+          onComplete: onComplete,
+          exerciseText: 'Next Exercise:',
+          taskText: 'REST',
+          stateDuration: state.workout.restDuration.inSeconds,
+          exercises: exercises,
+          controller: controller,
         );
+        return _buildPage(pageParams);
       } else if (state is ExerciseInProgressState) {
         print(state);
         final Function onComplete = () {
@@ -154,107 +66,17 @@ class WorkoutPage extends StatelessWidget {
             );
           }
         };
-        return PageAnimationWidget(
-          body: Container(
-            width: MediaQuery.of(context).size.width,
-            color: Color(0xff424242),
-            child: Column(
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text(
-                          'Current Exercise:',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Color(0xfffbc02d),
-                          ),
-                        ),
-                        Text(
-                          ('${exercises.last.title}'),
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      height: 2.0,
-                      width: MediaQuery.of(context).size.width,
-                      color: Colors.purple,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'WORK',
-                          style: TextStyle(
-                            fontSize: 45,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                CircleTimer(
-                  duration: state.workout.exerciseDuration.inSeconds,
-                  onComplete: onComplete,
-                  controller: controller,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    IconButton(
-                      onPressed: controller.restart,
-                      icon: Icon(
-                        Icons.replay_rounded,
-                        color: Colors.purple,
-                      ),
-                      iconSize: 75.0,
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        exercises.removeLast();
-                        if (exercises.isEmpty) {
-                          BlocProvider.of<WorkoutBloc>(context)
-                              .add(FinishWorkoutEvent(state.workout));
-                        } else {
-                          BlocProvider.of<WorkoutBloc>(context)
-                              .add(SkipWorkoutEvent(state.workout));
-                          controller.restart(
-                            duration: 10,
-                          );
-                        }
-                      },
-                      icon: Icon(
-                        Icons.skip_next_rounded,
-                        color: Colors.purple,
-                      ),
-                      iconSize: 75.0,
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    FormattedButton(
-                      onPressed: () {
-                        BlocProvider.of<WorkoutBloc>(context)
-                            .add(FinishWorkoutEvent(state.workout));
-                      },
-                      buttonText: 'Finish Workout',
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
+        final pageParams = Params(
+          context: context,
+          workout: state.workout,
+          onComplete: onComplete,
+          exerciseText: 'Current Exercise:',
+          taskText: 'WORK',
+          stateDuration: state.workout.exerciseDuration.inSeconds,
+          exercises: exercises,
+          controller: controller,
         );
+        return _buildPage(pageParams);
       } else if (state is SkipExerciseBufferState) {
         print(state);
         final Function onComplete = () {
@@ -264,107 +86,17 @@ class WorkoutPage extends StatelessWidget {
             duration: state.workout.exerciseDuration.inSeconds,
           );
         };
-        return PageAnimationWidget(
-          body: Container(
-            width: MediaQuery.of(context).size.width,
-            color: Color(0xff424242),
-            child: Column(
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text(
-                          'Next Exercise:',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Color(0xfffbc02d),
-                          ),
-                        ),
-                        Text(
-                          ('${exercises.last.title}'),
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      height: 2.0,
-                      width: MediaQuery.of(context).size.width,
-                      color: Colors.purple,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'GET READY',
-                          style: TextStyle(
-                            fontSize: 45,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                CircleTimer(
-                  duration: 10,
-                  onComplete: onComplete,
-                  controller: controller,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    IconButton(
-                      onPressed: controller.restart,
-                      icon: Icon(
-                        Icons.replay_rounded,
-                        color: Colors.purple,
-                      ),
-                      iconSize: 75.0,
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        exercises.removeLast();
-                        if (exercises.isEmpty) {
-                          BlocProvider.of<WorkoutBloc>(context)
-                              .add(FinishWorkoutEvent(state.workout));
-                        } else {
-                          BlocProvider.of<WorkoutBloc>(context)
-                              .add(SkipWorkoutEvent(state.workout));
-                          controller.restart(
-                            duration: 10,
-                          );
-                        }
-                      },
-                      icon: Icon(
-                        Icons.skip_next_rounded,
-                        color: Colors.purple,
-                      ),
-                      iconSize: 75.0,
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    FormattedButton(
-                      onPressed: () {
-                        BlocProvider.of<WorkoutBloc>(context)
-                            .add(FinishWorkoutEvent(state.workout));
-                      },
-                      buttonText: 'Finish Workout',
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
+        final pageParams = Params(
+          context: context,
+          workout: state.workout,
+          onComplete: onComplete,
+          exerciseText: 'Next Exercise:',
+          taskText: 'GET READY',
+          stateDuration: 10,
+          exercises: exercises,
+          controller: controller,
         );
+        return _buildPage(pageParams);
       } else {
         return PageAnimationWidget(
           body: Container(
@@ -379,4 +111,136 @@ class WorkoutPage extends StatelessWidget {
       }
     });
   }
+}
+
+class Params {
+  final BuildContext context;
+  final Workout workout;
+  final Function onComplete;
+  final String exerciseText;
+  final String taskText;
+  final int stateDuration;
+  final int bufferDuration;
+  final List<Exercise> exercises;
+  final CountDownController controller;
+
+  Params({
+    @required this.context,
+    @required this.workout,
+    @required this.onComplete,
+    @required this.exerciseText,
+    @required this.taskText,
+    @required this.stateDuration,
+    this.bufferDuration = 10,
+    @required this.exercises,
+    @required this.controller,
+  });
+}
+
+Widget _buildPage(
+  final Params pageParams,
+) {
+  return PageAnimationWidget(
+    body: Container(
+      width: MediaQuery.of(pageParams.context).size.width,
+      color: Color(0xff424242),
+      child: Column(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    pageParams.exerciseText,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Color(0xfffbc02d),
+                    ),
+                  ),
+                  Text(
+                    ('${pageParams.exercises.last.title}'),
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                height: 2.0,
+                width: MediaQuery.of(pageParams.context).size.width,
+                color: Colors.purple,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    pageParams.taskText,
+                    style: TextStyle(
+                      fontSize: 45,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          CircleTimer(
+            duration: pageParams.stateDuration,
+            onComplete: pageParams.onComplete,
+            controller: pageParams.controller,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              // restart button
+              IconButton(
+                onPressed: pageParams.controller.restart,
+                icon: Icon(
+                  Icons.replay_rounded,
+                  color: Colors.purple,
+                ),
+                iconSize: 75.0,
+              ),
+              // skip exercise button
+              IconButton(
+                onPressed: () {
+                  pageParams.exercises.removeLast();
+                  if (pageParams.exercises.isEmpty) {
+                    BlocProvider.of<WorkoutBloc>(pageParams.context)
+                        .add(FinishWorkoutEvent(pageParams.workout));
+                  } else {
+                    BlocProvider.of<WorkoutBloc>(pageParams.context)
+                        .add(SkipWorkoutEvent(pageParams.workout));
+                    pageParams.controller.restart(
+                      duration: pageParams.bufferDuration,
+                    );
+                  }
+                },
+                icon: Icon(
+                  Icons.skip_next_rounded,
+                  color: Colors.purple,
+                ),
+                iconSize: 75.0,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FormattedButton(
+                onPressed: () {
+                  BlocProvider.of<WorkoutBloc>(pageParams.context)
+                      .add(FinishWorkoutEvent(pageParams.workout));
+                },
+                buttonText: 'Finish Workout',
+              ),
+            ],
+          ),
+        ],
+      ),
+    ),
+  );
 }
