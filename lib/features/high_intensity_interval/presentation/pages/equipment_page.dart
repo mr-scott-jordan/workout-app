3cde05c53b757ab5babdd01c249026534df06d58
Scott Jordan <jordan.scott.email@gmail.com>
2021-04-10 19:08:41 -0700

    fixed imports

--- lib/features/high_intensity_interval/presentation/pages/equipment_page.dart
+++ lib/features/high_intensity_interval/presentation/pages/equipment_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../../../../core/authentication/bloc/user_bloc.dart';
 
+import '../../../../core/authentication/bloc/user_bloc.dart';
 import '../../domain/enums/equipment.dart';
 import '../bloc/workout_bloc.dart';
 import '../widgets/formatted_button.dart';
